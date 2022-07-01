bda774345a081f0d6bbc47cea2aa2d8ae17b3223
divicodes <divinef837@gmail.com>
2022-07-01 10:14:17 +0100

    added book controllers and route

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+
+const bookRouter = require('./routes/bookRoutes');
+
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// body parser
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use('/api/v1/books', bookRouter);
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
 
 module.exports = app;
---
+++ src/controllers/bookController.js
@@ -0,0 +1,40 @@
+const Book = require('../models/bookModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createBook = catchAsync(async (req, res, next) => {
+  const book = await Book.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      book,
+    },
+  });
+});
+
+exports.getBooks = catchAsync(async (req, res, next) => {
+  const books = await Book.find();
+
+  res.status(200).json({
+    status: 'success',
+    result: books.length,
+    data: {
+      books,
+    },
+  });
+});
+
+exports.getBook = catchAsync(async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+
+  if (!book) {
+    return next(new AppError('No book with the given ID was found', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      book,
+    },
+  });
+});
